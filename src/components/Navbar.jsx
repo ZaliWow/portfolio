5169e405b2d5edac21c49c09bd18be327cff17fd
David <morales.william@correounivalle.edu.co>
2024-01-26 10:13:20 -0500

    qlq

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export function Navbar({}) {
 <div>
 <div className="nav">
 
-<img src="src/images/Foto_perfil.jpg" alt="mi foto de perfil" />
+<img src="src\images\Foto_perfil.jpg" alt="mi foto de perfil" />
 
 <div className="me">
     <h2>Hola, me llamo David.</h2>
