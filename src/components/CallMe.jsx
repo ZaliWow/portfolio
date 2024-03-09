77dbe30ff5bf10c42371bd2632da89c904c30715
David <morales.william@correounivalle.edu.co>
2024-03-09 11:28:34 -0500

    update and fix CV

--- src/components/CallMe.jsx
+++ src/components/CallMe.jsx
@@ -14,6 +14,7 @@ export function CallMe(params) {
 
 
 const handleDownload =()=>{
+    
   const pdfUrl = "/achives/CV - William David Morales Pineda.pdf"
     const link = document.createElement('a')
     link.href= pdfUrl;
